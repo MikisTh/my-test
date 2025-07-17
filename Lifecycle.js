5bb9d4d37fd4f29bbf87ecff51211b52690a07a8
MikisTh <114635629+MikisTh@users.noreply.github.com>
2025-07-17 14:52:12 -0300

    Add files via upload

---
+++ Lifecycle.js
@@ -0,0 +1,9 @@
+
+//Lifecycle
+componentWillUnmount()
+componentWillMount()
+componentDidMount()
+componentDidUpdate()
+componentDidCatch()
+UNSAFE_componentWillMount()
+UNSAFE_componentWillUpdate()
